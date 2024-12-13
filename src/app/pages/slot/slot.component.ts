fd5fd83c0db8cb2ef644e43ab12a1d40ba70b8bf
afanalfiandi <alfiandiafan@gmail.com>
2024-12-14 00:41:30 +0700

    [add] pages

---
+++ src/app/pages/slot/slot.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slot',
+  templateUrl: './slot.component.html',
+  styleUrl: './slot.component.css'
+})
+export class SlotComponent {
+
+}
---
+++ src/app/pages/transaction-status/transaction-status.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-status',
+  templateUrl: './transaction-status.component.html',
+  styleUrl: './transaction-status.component.css'
+})
+export class TransactionStatusComponent {
+
+}
